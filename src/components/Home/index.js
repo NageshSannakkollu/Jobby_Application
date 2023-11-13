39f6b5c64f107f01700b74d5435a8f0ddbd5c39b
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2023-11-13 23:42:53 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,23 @@
-import {Component} from 'react'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Header from '../Header'
 import './index.css'
 
-class Home extends Component {
-  clickToFindJobs = () => {
-    const {history} = this.props
-    history.replace('/jobs')
-  }
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <div className="home-details-container">
+      <h1 className="find-jobs-heading">Find The Job That Fits Your Life</h1>
+      <p className="jobs-intro-description">
+        Millions of people are searching for jobs, salary information, company
+        reviews. Find the job that fits your abilities and potential.
+      </p>
+      <Link to="/jobs">
+        <button type="button" className="find-jobs-button">
+          Find Jobs
+        </button>
+      </Link>
+    </div>
+  </div>
+)
 
-  render() {
-    const jwtToken = Cookies.get('jwt_token')
-    if (jwtToken === undefined) {
-      return <Redirect to="/login" />
-    }
-    return (
-      <div className="home-container">
-        <Header />
-        <div className="home-inside-container">
-          <h1>Find The Job That Fits Your Life</h1>
-          <p>
-            Millions of people are searching for jobs, salary <br />
-            information, company reviews. Find the job that fits your <br />
-            abilities and potential.
-          </p>
-          <button
-            type="button"
-            className="find-jobs-button"
-            onClick={this.clickToFindJobs}
-          >
-            Find Jobs
-          </button>
-        </div>
-      </div>
-    )
-  }
-}
 export default Home
