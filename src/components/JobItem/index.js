1c25fc7ce51d495f90dd9fb06e070622813faf08
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2023-11-13 23:41:36 +0530

    Update index.js

--- src/components/JobItem/index.js
+++ src/components/JobItem/index.js
@@ -1,5 +1,5 @@
-import {ImLocation} from 'react-icons/im'
-import {BsBagFill} from 'react-icons/bs'
+import {AiFillStar} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
 import {Link} from 'react-router-dom'
 
 import './index.css'
@@ -8,64 +8,49 @@ const JobItem = props => {
   const {jobDetails} = props
   const {
     id,
-    location,
+    companyLogoUrl,
+    employmentType,
     jobDescription,
+    location,
     title,
     packagePerAnnum,
-    employmentType,
     rating,
-    companyLogoUrl,
   } = jobDetails
 
-  const getFinalPackage = packageAnnum => {
-    if (packageAnnum[1] === 'LPA') {
-      return packageAnnum[0] * 100000
-    }
-    return null
-  }
-  const packageAnnum = packagePerAnnum.split(' ')
-  const finalPackage = getFinalPackage(packageAnnum)
-  console.log(finalPackage)
-
   return (
-    <Link to={`/jobs/${id}`} className="link-item">
-      <li className="jobs-list" key={id}>
-        <div className="company-logo-title-rating-container">
+    <Link to={`/jobs/${id}`} className="jobs-link">
+      <li className="jobs-list-container">
+        <div className="logo-title-container">
           <img
             src={companyLogoUrl}
-            alt={title}
-            className="company-logo-image"
+            alt="company logo"
+            className="company-logo"
           />
           <div className="title-rating-container">
-            <h3>{title}</h3>
-            <div className="star-rating-container">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/star-filled-img.png"
-                alt="star"
-                className="rating-star-image"
-              />
-              <p className="rating">{rating}</p>
+            <h1 className="title">{title}</h1>
+            <div className="rating-container">
+              <AiFillStar className="star" />
+              <p>{rating}</p>
             </div>
           </div>
         </div>
-        <div className="location-type-package-container">
-          <div className="location-employment-type-container">
+        <div className="location-employment-package-container">
+          <div className="location-employment-container">
             <div className="location-container">
-              <ImLocation />
-              <p>{location}</p>
-            </div>
-            <div className="location-container">
-              <BsBagFill />
-              <p>{employmentType}</p>
+              <p className="location">{location}</p>
+              <div className="employment-container">
+                <BsBriefcaseFill />
+                <p className="employment-type">{employmentType}</p>
+              </div>
             </div>
           </div>
-          <p>{packagePerAnnum}/-</p>
+          <p>{packagePerAnnum}</p>
         </div>
         <hr />
+        <h3>Description</h3>
         <p>{jobDescription}</p>
       </li>
     </Link>
   )
 }
-
 export default JobItem
