af279a118d5812f289959d53a275fe556a75b9b4
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2023-11-13 23:44:17 +0530

    Update index.js

--- src/components/SimilarJobsItem/index.js
+++ src/components/SimilarJobsItem/index.js
@@ -1,49 +1,49 @@
-import {ImLocation} from 'react-icons/im'
-import {BsBagFill} from 'react-icons/bs'
-
+import {AiFillStar} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
 import './index.css'
 
-const SimilarJobsItem = props => {
+const SimilarJobsCard = props => {
   const {similarJobDetails} = props
   const {
     companyLogoUrl,
+    employmentType,
     title,
     rating,
     location,
     jobDescription,
-    employmentType,
+    id,
   } = similarJobDetails
-
   return (
-    <li className="similar-jobs-container">
-      <div className="company-logo-title-rating-container">
-        <img src={companyLogoUrl} alt={title} className="company-logo-image" />
+    <li className="jobs-list-container" key={id}>
+      <div className="logo-title-container">
+        <img
+          src={companyLogoUrl}
+          alt="similar job company logo"
+          className="company-logo"
+        />
         <div className="title-rating-container">
-          <h3 className="title">{title}</h3>
-          <div className="star-rating-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/star-filled-img.png"
-              alt="star"
-              className="rating-star-image"
-            />
-            <p className="rating">{rating}</p>
+          <h1 className="title">{title}</h1>
+          <div className="rating-container">
+            <AiFillStar className="star" />
+            <p>{rating}</p>
           </div>
         </div>
       </div>
       <h3>Description</h3>
       <p>{jobDescription}</p>
-      <div className="location-employment-type-container">
-        <div className="location-container">
-          <ImLocation />
-          <p>{location}</p>
-        </div>
-        <div className="location-container">
-          <BsBagFill />
-          <p>{employmentType}</p>
+      <div className="location-employment-package-container">
+        <div className="location-employment-container">
+          <div className="location-container">
+            <p className="location">{location}</p>
+            <div className="employment-container">
+              <BsBriefcaseFill />
+              <p className="employment-type">{employmentType}</p>
+            </div>
+          </div>
         </div>
       </div>
     </li>
   )
 }
 
-export default SimilarJobsItem
+export default SimilarJobsCard
